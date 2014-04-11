1626f4ba3f9c6f333e807dec0af3f4c108016f72
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2014-04-10 23:43:34 -0400

    removed some old stuff

--- runCfmDiJetHist.C
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-void runCfmDiJetHist()
-{
-  gROOT->ProcessLine(".L cfmDiJetHist.C++");
-    
-  gROOT->ProcessLine("cfmDiJetHist(\"Pythia80_HydjetDrum_mix01_HiForest2_v20_CFMSKIM.root\", 1, \"Pythia80_HydjetDrum_mix01_HiForest2_v20_CFMHIST.root\")");
-}
